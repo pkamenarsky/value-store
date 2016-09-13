993a5e3b368f58f855ad595dffd88348cae06ec0
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-09-13 12:50:40 +0300

    Book instances

--- Database/Bookkeeper.hs
+++ Database/Bookkeeper.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Database.Bookkeeper where
@@ -6,10 +10,40 @@ module Database.Bookkeeper where
 import Bookkeeper
 import Bookkeeper.Internal
 
+import Data.Proxy
+
+import qualified Data.ByteString.Char8 as BC
+
 import Database.Generic
 
-instance Fields (Book' kvs) where
-  fields = undefined
+import qualified Database.PostgreSQL.Simple.FromField as PS
+import qualified Database.PostgreSQL.Simple.ToField as PS
+import qualified Database.PostgreSQL.Simple.FromRow as PS
+import qualified Database.PostgreSQL.Simple.ToRow as PS
+import qualified Database.PostgreSQL.Simple.Internal as PS
+
+import qualified Data.Type.Map as Map
+
+import GHC.OverloadedLabels
+import GHC.TypeLits
+
+instance Fields (Book' '[]) where
+  fields _ = Object []
   cnst = undefined
   cnstM = undefined
   cnstS = undefined
+
+instance (KnownSymbol k, Fields (Book' m)) => Fields (Book' (k :=> String ': m)) where
+  fields (Just (Book (Map.Ext k v m))) = Object [(symbolVal k, Value $ PS.Escape $ BC.pack v)]
+  cnst = undefined
+  cnstM = undefined
+  cnstS = undefined
+
+instance {-# OVERLAPPABLE #-} (KnownSymbol k, Fields v, Fields (Book' m)) => Fields (Book' (k :=> v ': m)) where
+  fields (Just (Book (Map.Ext k v m))) = Object [(symbolVal k, fields (Just v))]
+  cnst = undefined
+  cnstM = undefined
+  cnstS = undefined
+
+test_fields :: Object PS.Action
+test_fields = fields (Just (emptyBook & #name =: "name_value"))
